1a79b9dc9359ca6372a28e82ec120223eefb87af
Roger Guldbrandsen <roger@kinbiko.com>
2018-02-25 14:46:38 +0000

    Extract js to own file

--- index.js
+++ index.js
@@ -8,3 +8,31 @@ function changeCarpark() {
   document.getElementById('output').innerHTML =
     "We think it's likely that " + carpark + ' is ' + modelResult + ' % full';
 }
+
+function initMap() {
+  var Coordinates = { lat: lat, lng: long };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+    center: Coordinates
+  });
+  var marker = new google.maps.Marker({
+    position: Coordinates,
+    map: map
+  });
+}
+
+const carparks = [
+  { name: 'Odd Down P+R', long: -2.38389427175, lat: 51.352935229 },
+  { name: 'SouthGate Rail CP', long: -2.35939373665, lat: 51.3783156754 },
+  { name: 'Lansdown P+R', long: -2.38697144144, lat: 51.4113171399 },
+  { name: 'Charlotte Street CP', long: -2.36860892385, lat: 51.3843384358 },
+  { name: 'Podium CP', long: -2.35906657317, lat: 51.3842422001 },
+  { name: 'Avon Street CP', long: -2.36225609279, lat: 51.3787114813 },
+  { name: 'SouthGate General CP', long: -2.35893376096, lat: 51.3782901111 },
+  { name: 'Newbridge P+R', long: -2.40590782211, lat: 51.3902305335 }
+];
+
+let lat = carparks[0].lat;
+let long = carparks[0].long;
+
+document.getElementById('CarParkLong').innerHTML = CarParks[0][0];
